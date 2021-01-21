960745bd0ddf3fc382765290d3486f2811f0e901
Diego Ferreira Gonçalves <ferreira.dfg@gmail.com>
2021-01-21 11:40:31 -0300

    modules

---
+++ modules/read_module.py
@@ -0,0 +1,4 @@
+import util as ut
+
+ut.string_msg('Digite um nome: ')
+ut.float_msg('Digite um numero: ')
---
+++ modules/util.py
@@ -0,0 +1,5 @@
+def string_msg(a):
+    return input(a)
+
+def float_msg(b):
+    return float(input(b))
