4d50e248d1fe90824213e809e03851738c69d099
Diego Ferreira Gonçalves <ferreira.dfg@gmail.com>
2021-03-01 13:20:17 -0300

    unordered array insert function

---
+++ unorderedarray /array.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+class VetorNaoOrdenado:
+    def __init__(self, capacidade):
+        self.capacidade = capacidade
+        self.ultima_posicao = -1
+        self.valores = np.empty(self.capacidade, dtype=int)
+
+    def prints(self):
+        if self.ultima_posicao == -1:
+            print('O vetor está vazio')
+        else:
+            for i in range(self.ultima_posicao +1):
+                print(i,'-',self.valores[i])
+
+vetor = VetorNaoOrdenado(5)
+
+vetor.prints()
