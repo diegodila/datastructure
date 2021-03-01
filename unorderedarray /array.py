7195a6dc3d4bded77fcd1edf4f04454ddd8d4b09
Diego Ferreira Gonçalves <ferreira.dfg@gmail.com>
2021-03-01 13:22:11 -0300

    adding insert function array file

--- unorderedarray /array.py
+++ unorderedarray /array.py
@@ -6,12 +6,21 @@ class VetorNaoOrdenado:
         self.ultima_posicao = -1
         self.valores = np.empty(self.capacidade, dtype=int)
 
+    # O (n)
     def prints(self):
         if self.ultima_posicao == -1:
             print('O vetor está vazio')
         else:
             for i in range(self.ultima_posicao +1):
-                print(i,'-',self.valores[i])
+                print(f'Posicão: {i} - valor: {self.valores[i]}')
+
+    # O(1) - O (2)
+    def insert(self, valor):
+        if self.ultima_posicao == self.capacidade -1:
+            print('Capacidade maxima atingida')
+        else:
+            self.ultima_posicao += 1
+            self.valores[self.ultima_posicao] = valor
 
 vetor = VetorNaoOrdenado(5)
 
