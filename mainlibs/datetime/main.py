26ab7fd0c30ac31a2f05ef1bf4f584d6be39044e
Diego Ferreira Gonçalves <ferreira.dfg@gmail.com>
2022-01-04 23:42:06 -0300

    adding datetime lib

--- mainlibs/datetime/main.py
+++ mainlibs/datetime/main.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 type(datetime)
 dir(datetime) #recursos
-datetime.date.today() #today
+datetime.date.today() #today Python get today's date
 datetime.now() #now
 now = datetime.now()
 now
@@ -12,3 +12,39 @@ data
 data.day #day
 data.month #mouth
 data.year #year
+
+
+#Current date in different formats
+
+from datetime import date
+
+today = date.today()
+today
+
+# dd/mm/YY
+d1 = today.strftime("%d/%m/%Y")
+print("d1 =", d1)
+
+# Textual month, day and year	
+d2 = today.strftime("%B %d, %Y")
+print("d2 =", d2)
+
+# mm/dd/y
+d3 = today.strftime("%m/%d/%y")
+print("d3 =", d3)
+
+# Month abbreviation, day and year	
+d4 = today.strftime("%b-%d-%Y")
+print("d4 =", d4)
+
+#Get the current date and time
+from datetime import datetime
+
+# datetime object containing current date and time
+now = datetime.now()
+ 
+print("now =", now)
+
+# dd/mm/YY H:M:S
+dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+print("date and time =", dt_string)
