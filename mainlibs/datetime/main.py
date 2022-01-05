8cc8719f29544222887bb1b294daf06121510570
Diego Ferreira Gonçalves <ferreira.dfg@gmail.com>
2022-01-04 23:38:02 -0300

    adding datetime lib

---
+++ mainlibs/datetime/main.py
@@ -0,0 +1,14 @@
+import datetime
+from datetime import datetime
+
+type(datetime)
+dir(datetime) #recursos
+datetime.date.today() #today
+datetime.now() #now
+now = datetime.now()
+now
+data = datetime.date(2020,7,10)
+data
+data.day #day
+data.month #mouth
+data.year #year
