313752e71f17df30738fd622f84dbd3f18836c6e
Diego Ferreira Gonçalves <ferreira.dfg@gmail.com>
2022-01-05 00:27:28 -0300

    adding module os

--- mainlibs/math/main.py
+++ mainlibs/math/main.py
@@ -3,8 +3,8 @@ import math
 math.sqrt(36) #root
 math.sin(45) #seno
 math.cos(45) #coseno
-math.log(1000,10) //logaritimo
-math.log(32,2) 
+math.log(1000,10) #logarithmic
+math.log(32,2)
 math.log(1000)
-math.e //euller
-math.pi //Pi
+math.e #euller
+math.pi #Pi
---
+++ mainlibs/os/main.py
@@ -0,0 +1,7 @@
+import os
+print(os.getcwd())
+os.chdir("./sets")
+os.getcwd()
+os.system('ls -l')
+os.system('ls -a')
+os.system("echo Hello from the other side! >> tex")
