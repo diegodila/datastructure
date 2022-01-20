a405ff97a00046009d313a6f23c5a27f89f10112
Diego Ferreira Gonçalves <ferreira.dfg@gmail.com>
2022-01-20 09:22:24 -0300

    Python Arithmetic Operators

---
+++ 1_variables/10_math.py
@@ -0,0 +1,30 @@
+#Python Arithmetic Operators
+number_sum  = 1 + 2 
+number_a = 3
+print(f'Sum: {number_sum + number_a} ')
+
+number_minus = 3 - 2
+number_b = 1
+print(f'Minus: {number_minus - number_b}')
+
+number_mult = 2 * 2 
+number_c = 2
+print(f'Mult: {number_mult * number_c} ')
+
+number_div = 10 / 2
+number_d = 2
+print(f'Div: {number_div / number_d} ')
+
+
+
+#//: Divides the number on its left by the number on its right, rounds down the answer, and returns a whole number.
+number_div_right = 5 // 2
+print(f'Floor division: {number_div_right} ')
+
+#Modulus
+modulus_div = 5 % 2
+print(f'Modulus div 5 % 2: {modulus_div}') 
+
+#Exponentiation
+number_exp = 2**2
+print(f'Exp: {number_exp}')
